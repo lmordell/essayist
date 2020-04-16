b0c896ab31a44f9609db60d509a880a583dd5d81
Lee Mordell <lmordell@intellisurvey.com>
2020-04-15 19:35:08 -0700

    add requirements to requirements.txt

--- server/api.py
+++ server/api.py
@@ -2,11 +2,17 @@ from flask import Flask
 from flask_restful import Api, Resource
 from flask_cors import CORS
 from app import Essayist
+import nltk
+
+# Download nltk dependencies
+nltk.download('treebank')
+nltk.download('punkt')
 
 app = Flask(__name__)
 CORS(app)
 
 api = Api(app)
+
 
 api.add_resource(Essayist, '/get_essay_analysis')
 
