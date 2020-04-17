8e403cd7aacca82f3546d2985af046d0ddf8a2f7
Lee Mordell <lmordell@intellisurvey.com>
2020-04-16 18:49:34 -0700

    fix app naming conflict

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn --pythonpath app api:app --log-file -
+web: gunicorn --pythonpath app api:app --log-file - debug
--- app/api.py
+++ app/api.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template
 from flask_restful import Api, Resource
 from flask_cors import CORS
-from app import Essayist
+from essayist import Essayist
 import nltk
 
 # Download nltk dependencies
@@ -16,7 +16,6 @@ api = Api(app)
 @app.route("/")
 def start():
     return render_template("index.html")
-
 
 api.add_resource(Essayist, '/get_essay_analysis')
 
